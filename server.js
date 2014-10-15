7f17232b69b5ff04ab4c090ebfdde626bb6d877d
Tim Park <timfpark@gmail.com>
2014-10-15 12:47:01 -0700

    more output so we can see logs

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ var http = require('http');
 
 var startTime = new Date();
 
+var PORT = 8000;
+
 var server = http.createServer(function(req, res){
     // for any request, print out the number of milliseconds since the server started.
 
     var elapsedTime = new Date().getTime() - startTime.getTime();
+
+    console.log('request received, sending ' + elapsedTime.toString());
     res.end(elapsedTime.toString());
 });
 
-server.listen(8000);
+server.listen(PORT);
+
+console.log('simple node.js server started and listening on port: ' + PORT);
